0ae2151b19cf15ac6b3491551941b273e1487f0b
Abhirami Sasikumar <abhiramiabhiramam1@gmail.com>
2025-03-28 15:45:12 +0530

    Chart component added

--- App.js
+++ App.js
@@ -7,20 +7,21 @@ import Login from './Login/Login';
 import ClusterName from './ClusterName/ClusterName';
 import Parameters from './ParameterScreen/Parameters';
 import LocationScreen from './LocationScreen/Location';
-
+import ChartComponent from './Chart/ChartComponent';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="ClusterName" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="Chart" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="Otp" component={Otp} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="ClusterName" component={ClusterName} />
         <Stack.Screen name="Parameters" component={Parameters} />
         <Stack.Screen name="Location" component={LocationScreen} />
+        <Stack.Screen name="Chart" component={ChartComponent} />
         
 
       </Stack.Navigator>
