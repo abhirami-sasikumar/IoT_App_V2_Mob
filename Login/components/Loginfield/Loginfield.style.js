5ee719b42104d7f67ae202f9473bd12ad409049e
alameen_nizam <alameennizam1@gmail.com>
2025-08-14 11:06:07 +0530

    all the bugs are fixed

--- Login/components/Loginfield/Loginfield.style.js
+++ Login/components/Loginfield/Loginfield.style.js
@@ -4,7 +4,7 @@ import { scale, verticalScale } from "react-native-size-matters";
 const styles = StyleSheet.create({
   view: {
 
-    marginTop: verticalScale(290),
+    marginTop: verticalScale(250),
 
 
     justifyContent: "flex-start",
